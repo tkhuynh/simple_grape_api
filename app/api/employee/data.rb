c3df99f6e31d20ebf9f81dda4fddcf52ab710f04
tkhuynh <thequest2311@yahoo.com>
2017-08-01 14:44:45 -0700

    create new employee

--- app/api/employee/data.rb
+++ app/api/employee/data.rb
@@ -2,10 +2,31 @@ module Employee
 	class Data < Grape::API
 
 		resource :employee_datas do
+
 			desc 'List all employees'
 
 			get do
 				Emp.all
+			end
+
+		end
+
+		resource :employee_data do
+
+			desc 'create new employee'
+
+			params do
+			  requires :name, type: String
+			  requires :sport, type:String
+			  requires :age, type:Integer
+			end
+
+			post do
+			  Emp.create!({
+			    name:params[:name],
+			    sport:params[:sport],
+			    age:params[:age]
+			  })
 			end
 
 		end
